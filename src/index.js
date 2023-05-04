a57e027afdd63bb96ca48edcca43ffc27295a297
Shabbir1065 <shabbir1065@gmail.com>
2023-05-04 17:48:56 -0400

    connected to mongodb. was only like 1 line of code

--- src/index.js
+++ src/index.js
@@ -7,4 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect("mongodb+srv://shabbir1065:MERNproject@financials.cx6ol15.mongodb.net/financials?retryWrites=true&w=majority");
+
 app.listen(3001, () => console.log("Server is up and running"));
