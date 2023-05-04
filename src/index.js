e832998897e71157fc142f8b6991e7dcc148d2eb
Shabbir1065 <shabbir1065@gmail.com>
2023-05-04 19:27:56 -0400

    basically created the loggin in functionality within the users.js file in routes folder

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import {userRouter} from "./routes/users.js"
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect("mongodb+srv://shabbir1065:MERNproject@financials.cx6ol15.mongodb.net/financials?retryWrites=true&w=majority");
 
