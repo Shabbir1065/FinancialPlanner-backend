cc4c6c291179471a905ef33dfad167c6c5156430
Shabbir1065 <shabbir1065@gmail.com>
2023-05-12 18:27:17 -0400

    changing the index.js back to what it was

--- src/index.js
+++ src/index.js
@@ -7,18 +7,7 @@ import { userRouter } from "./routes/users.js"
 const app = express();
 
 app.use(express.json());
-const whitelist = ["https://financialplanner-api.onrender.com"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
+app.use(cors());
 
 app.use("/api", userRouter);
 
